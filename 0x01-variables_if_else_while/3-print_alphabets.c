30892acaf194cce4eb3642ab5e15ce946a15a2a6
Damilola <ipinmisho.damilola@gmail.com>
2022-10-20 09:18:30 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,24 +2,17 @@
 
 /**
  * main - Prints the alphabet in lowercase, and then in uppercase
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-
-		putchar(letter);
-
-	for (letter = 'A'; letter <= 'Z'; letter++)
-
-		putchar(letter);
-
-	putchar('\n');
-
-	return (0);
-
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+for (letter = 'A'; letter <= 'Z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
 }
