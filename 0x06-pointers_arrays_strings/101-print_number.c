e2a15d4bd709e488afd339d9f1ecf916df6523db
Damilola <ipinmisho.damilola@gmail.com>
2022-10-28 02:23:49 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,17 +8,18 @@
 void print_number(int n)
 {
 unsigned int n1;
+
 if (n < 0)
 {
 n1 = -n;
 _putchar('-');
-}
-else
+} else
 {
 n1 = n;
-i}
+}
+
 if (n1 / 10)
 print_number(n1 / 10);
-_putchar((n1 % 10) + '0');
 
+_putchar((n1 % 10) + '0');
 }
